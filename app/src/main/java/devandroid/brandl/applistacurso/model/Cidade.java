46d9036bc29d20f88072aa7188a0dd66781d9b2b
Allan Brandl <allanbrandlti@gmail.com>
2025-02-01 09:58:35 -0300

    Uso dos metodos Gets e Sets.

---
+++ app/src/main/java/devandroid/brandl/applistacurso/model/Cidade.java
@@ -0,0 +1,37 @@
+package devandroid.brandl.applistacurso.model;
+
+public class Cidade {
+
+    private String nomeCidade;
+    private String estadoCidade;
+    private String paisCidade;
+
+
+
+    public Cidade() {
+    }
+
+    public String getNomeCidade() {
+        return nomeCidade;
+    }
+
+    public void setNomeCidade(String nomeCidade) {
+        this.nomeCidade = nomeCidade;
+    }
+
+    public String getEstadoCidade() {
+        return estadoCidade;
+    }
+
+    public void setEstadoCidade(String estadoCidade) {
+        this.estadoCidade = estadoCidade;
+    }
+
+    public String getPaisCidade() {
+        return paisCidade;
+    }
+
+    public void setPaisCidade(String paisCidade) {
+        this.paisCidade = paisCidade;
+    }
+}
